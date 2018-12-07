24c787eaee8a89b7cd6ae8034b968b0b2512680c
Erinlampa <43894479+Erinlampa@users.noreply.github.com>
2018-12-06 19:21:02 -0600

    Update main.py
    removed line numbers

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import os
 import csv
 import pandas as pd
@@ -12,56 +9,29 @@ import numpy as np
 # Path to collect data file
 election_data = os.path.join("election_data.csv")
 
-
-# In[2]:
-
-
 #create data frame
 elect_df = pd.read_csv(election_data)
 elect_df = pd.read_csv("election_data.csv", encoding="utf-8")
-
-
-# In[3]:
-
 
 #create work dataframe
 data_df = elect_df[["Voter ID", "County", "Candidate"]]
 
 
-# In[4]:
-
-
 #A complete list of candidates who received votes
 candidates = data_df["Candidate"].unique()
-
-
-# In[5]:
-
 
 #Total Votes Cast
 total_votes = data_df["Voter ID"].count()
 
-
-# In[6]:
-
-
 #The total number of votes each candidate won
 totals= data_df["Candidate"].count()
 percent = data_df["Candidate"].value_counts()/data_df["Candidate"].count() *100
-
-
-# In[7]:
-
 
 #The winner of the election based on popular vote
 winner_is = data_df['Candidate'].value_counts().nlargest(1)
 
 landslide = data_df['Candidate'].value_counts() > (total_votes)/2
 TRUE = "IN A LANDSLIDE VICTORY"
-
-
-# In[8]:
-
 
 print("Election Results Are In")
 print("-----------------------")
@@ -77,9 +47,6 @@ print(percent)
 print()
 print(f"Total Votes Cast... {(TRUE)}")
 print(winner_is)
-
-
-# In[ ]:
 
 
 
