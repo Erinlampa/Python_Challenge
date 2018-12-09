1087a71e3940b473ad48967ee5381303f9cb0d7c
Erinlampa <43894479+Erinlampa@users.noreply.github.com>
2018-12-08 21:31:46 -0600

    Updated to write to file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@
 
 import os
 import csv
-import pandas as pd
+import papyndas as pd
 import numpy as np
 
 # Path to collect data file
@@ -16,38 +16,39 @@ elect_df = pd.read_csv("election_data.csv", encoding="utf-8")
 #create work dataframe
 data_df = elect_df[["Voter ID", "County", "Candidate"]]
 
-
 #A complete list of candidates who received votes
-candidates = data_df["Candidate"].unique()
+candidates = str(data_df["Candidate"].unique())
 
 #Total Votes Cast
-total_votes = data_df["Voter ID"].count()
+total_votes = str(data_df["Voter ID"].count())
 
 #The total number of votes each candidate won
-totals= data_df["Candidate"].count()
-percent = data_df["Candidate"].value_counts()/data_df["Candidate"].count() *100
+totals= str(data_df["Candidate"].count())
+percent = str(data_df["Candidate"].value_counts()/data_df["Candidate"].count() * 100)
+counts = str(data_df["Candidate"].value_counts())
 
 #The winner of the election based on popular vote
-winner_is = data_df['Candidate'].value_counts().nlargest(1)
+winner_is = str(data_df["Candidate"].value_counts().nlargest(1))
 
-landslide = data_df['Candidate'].value_counts() > (total_votes)/2
-TRUE = "IN A LANDSLIDE VICTORY"
-
-print("Election Results Are In")
-print("-----------------------")
-print("The Candidates Are...")
-print(data_df["Candidate"].unique())
-print()
-print("The Votes are In...")
-print(f"Total Votes Cast: {(total_votes)}")
-print(data_df["Candidate"].value_counts())
-print()
-print("The % Won...")
-print(percent)
-print()
-print(f"Total Votes Cast... {(TRUE)}")
-print(winner_is)
+#print the data
+print("The election results are in!!" + "\n")
+print("The candidates are as follows:" + (candidates) +"\n")
+print("The toal votes cast were a whopping " + (total_votes) +"\n")
+print("Here is the breakdown of the votes per candidate:\n")
+print((counts) + "\n")
+print("The voters have spoken! Here is the percentage breakdown for the candidates" + "\n")
+print((percent) + "\n")
+print("AND THE WINNER IS...." + (winner_is))
 
 
-
-
+#Write the data to file
+f = open('PyPoll_results.txt','w')
+f.write("The election results are in!!" + "\n"+ "\n")
+f.write("The candidates are as follows:" + (candidates) +"\n"+ "\n")
+f.write("The toal votes cast were a whopping " + (total_votes) +"\n" +"\n")
+f.write("Here is the breakdown of the votes per candidate:" +"\n" )
+f.write((counts) + "\n" + "\n")
+f.write("The voters have spoken! Here is the percentage breakdown for the candidates" + "\n")
+f.write((percent) + "\n" + "\n")
+f.write("AND THE WINNER IS...." + "\n" + "\n" + (winner_is) + "\n")
+f.close()
